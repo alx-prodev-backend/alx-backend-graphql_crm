51b4af1716dd8c0adef7b33822e30a60a6d50b98
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-08-31 22:36:58 +0300

    Schema file

--- crm/schema.py
+++ crm/schema.py
@@ -1,28 +1,37 @@
 import graphene
-from .models import Product  # افترض إن عندك موديل اسمه Product
-from graphene_django import DjangoObjectType
+from .models import Product  #  Product
 
-class ProductType(DjangoObjectType):
-    class Meta:
-        model = Product
-        fields = ("id", "name", "stock")
 
+# GraphQL type
+class ProductType(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    stock = graphene.Int()
+
+
+#
 class UpdateLowStockProducts(graphene.Mutation):
-    success = graphene.String()
+    class Arguments:
+        pass  # no  input
+
     updated_products = graphene.List(ProductType)
+    message = graphene.String()
 
     def mutate(self, info):
         low_stock_products = Product.objects.filter(stock__lt=10)
-        updated = []
+        updated_products = []
+
         for product in low_stock_products:
-            product.stock += 10
+            product.stock += 10  # simulate restocking
             product.save()
-            updated.append(product)
+            updated_products.append(product)
 
         return UpdateLowStockProducts(
-            success="Low stock products updated successfully",
-            updated_products=updated
+            updated_products=updated_products,
+            message="Low stock products updated successfully!"
         )
 
+
+#
 class Mutation(graphene.ObjectType):
     update_low_stock_products = UpdateLowStockProducts.Field()
