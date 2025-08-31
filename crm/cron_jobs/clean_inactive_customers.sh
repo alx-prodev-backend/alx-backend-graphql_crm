275212f959e8a064b7d67893a095d4a104911a2c
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-08-31 21:15:41 +0300

    .sh update

--- crm/cron_jobs/clean_inactive_customers.sh
+++ crm/cron_jobs/clean_inactive_customers.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-LOGFILE="/tmp/customer_cleanup_log.txt"
-PROJECT_DIR="/home/osboxes/alx/alx-backend-graphql_crm"
 
-deleted_count=$(python3 $PROJECT_DIR/manage.py shell -c "
+PROJECT_DIR="/home/osboxes/alx/alx-backend-graphql_crm"
+LOGFILE="$PROJECT_DIR/crm/cron_jobs/clean_inactive_customers.log"
+
+
+cd $PROJECT_DIR || exit
+
+
+if [ -f ".venv/bin/activate" ]; then
+    source .venv/bin/activate
+fi
+
+
+deleted_count=$(python3 manage.py shell -c "
 from django.utils import timezone
 from crm.models import Customer
 from datetime import timedelta
 
 one_year_ago = timezone.now() - timedelta(days=365)
-qs = Customer.objects.filter(last_order_date__lt=one_year_ago)
+qs = Customer.objects.filter(last_active__lt=one_year_ago)
 count = qs.count()
 qs.delete()
 print(count)
 ")
+
+
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] Deleted customers: $deleted_count" >> $LOGFILE
