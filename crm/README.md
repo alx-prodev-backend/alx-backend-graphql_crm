9aa5054ffaa57a0e91c67663b45472daad1c0b32
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-08-31 23:00:49 +0300

    celery&& settings && README && tasks

---
+++ crm/README.md
@@ -0,0 +1,11 @@
+# CRM Celery Setup
+
+## Requirements
+- Redis
+- Celery
+- django-celery-beat
+
+## Setup
+1. Install dependencies:
+   ```bash
+   pip install -r requirements.txt
--- crm/settings.py
+++ crm/settings.py
@@ -1,4 +1,14 @@
+from celery.schedules import crontab
+
 CRONJOBS = [
     ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
     ('0 */12 * * *', 'crm.cron.update_low_stock'),
 ]
+
+
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
